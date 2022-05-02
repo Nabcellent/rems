5db8f13e2f3a2f17523edba36a655b4541a6f0b9
nabcellent <nabcellent.dev@gmail.com>
2022-05-02 11:17:41 +0300

    #2vxcc3p: Created migrations
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

---
+++ app/Enums/RoomType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum RoomType: string
+{
+    case BEDROOM = "BEDROOM";
+    case KITCHEN = "KITCHEN";
+    case TOILET = "TOILET";
+    case BATHROOM = "BATHROOM";
+}
