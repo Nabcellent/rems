c6fba5e2c3d8f777c2759d636301a9fc30d0b566
nabcellent <nabcellent.dev@gmail.com>
2022-05-02 11:13:44 +0300

    #2vxcc3p: Created migrations
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- database/migrations/2022_04_26_161439_create_payments_table.php
+++ database/migrations/2022_04_26_161439_create_payments_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId('transaction_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->morphs("payable"); // WALLET ID or MPESA CALLBACK ID or PAYPAL ID
             $table->bigInteger("amount");
