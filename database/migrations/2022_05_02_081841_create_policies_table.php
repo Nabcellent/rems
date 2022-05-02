1e45c3ac018259d98e7e044a8321380a0f85c82d
nabcellent <nabcellent.dev@gmail.com>
2022-05-02 11:29:24 +0300

    #2vxcc3p: Created migrations
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- database/migrations/2022_05_02_081841_create_policies_table.php
+++ database/migrations/2022_05_02_081841_create_policies_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('policies', function(Blueprint $table) {
             $table->id();
-            $table->morphs("entity"); // Estate/Property/Unit ID
+            $table->morphs("entity"); // Estate / Property / Unit ID
             $table->text("description")->fullText();
             $table->timestamps();
         });
