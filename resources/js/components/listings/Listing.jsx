83858eb2c6bd1f303aef8bc5421a358120c398b4
khalifa47 <khalifafumo5@gmail.com>
2022-07-09 14:19:53 +0300

    linked listing to single listings

--- resources/js/components/listings/Listing.jsx
+++ resources/js/components/listings/Listing.jsx
@@ -1,15 +1,36 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
 import Grid from "@mui/material/Grid";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 
 import { Carousel } from "react-bootstrap";
+import { Inertia } from "@inertiajs/inertia";
 
-const Listing = ({ imgSource, status, price, location, bedrooms, amenities }) => {
+const Listing = ({
+    id,
+    imgSource,
+    status,
+    price,
+    location,
+    bedrooms,
+    amenities,
+}) => {
     return (
-        <Grid container spacing={2} mt={'2rem'} justifyContent="center" alignItems="center" borderRadius={'4px'} pb={2} boxShadow={2}>
+        <Grid
+            component={CardActionArea}
+            onClick={() => Inertia.get(`/listings/${id}`)}
+            container
+            spacing={2}
+            mt={"2rem"}
+            justifyContent="center"
+            alignItems="center"
+            borderRadius={"4px"}
+            pb={2}
+            boxShadow={2}
+        >
             <Grid item xs={12} md={6}>
                 {/* <Carousel fade>
                     {imgSources.map((imgSource, i) => (
@@ -18,29 +39,94 @@ const Listing = ({ imgSource, status, price, location, bedrooms, amenities }) =>
                         </Carousel.Item>
                     ))}
                 </Carousel> */}
-                <Box component={'img'} src={imgSource} display={'block'} width={'100%'} height={'330px'} borderRadius={'4px'} sx={{ objectFit: 'cover' }} />
+                <Box
+                    component={"img"}
+                    src={imgSource}
+                    display={"block"}
+                    width={"100%"}
+                    height={"330px"}
+                    borderRadius={"4px"}
+                    sx={{ objectFit: "cover" }}
+                />
             </Grid>
             <Grid item xs={12} md={6}>
                 <Grid container spacing={1} columnSpacing={0}>
-                    <Grid item xs={6}><Typography variant="body1" fontWeight={600} fontSize={'1.1rem'}>STATUS</Typography></Grid>
-                    <Grid item xs={6}><Typography>{status.toUpperCase()}</Typography></Grid>
+                    <Grid item xs={6}>
+                        <Typography
+                            variant="body1"
+                            fontWeight={600}
+                            fontSize={"1.1rem"}
+                        >
+                            STATUS
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography>{status.toUpperCase()}</Typography>
+                    </Grid>
 
-                    <Grid item xs={6}><Typography variant="body1" fontWeight={600} fontSize={'1.1rem'}>PRICE</Typography></Grid>
-                    <Grid item xs={6}><Typography>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'KSH' }).format(price)}</Typography></Grid>
+                    <Grid item xs={6}>
+                        <Typography
+                            variant="body1"
+                            fontWeight={600}
+                            fontSize={"1.1rem"}
+                        >
+                            PRICE
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography>
+                            {new Intl.NumberFormat("en-US", {
+                                style: "currency",
+                                currency: "KSH",
+                            }).format(price)}
+                        </Typography>
+                    </Grid>
 
-                    <Grid item xs={6}><Typography variant="body1" fontWeight={600} fontSize={'1.1rem'}>LOCATION</Typography></Grid>
-                    <Grid item xs={6}><Typography>{location.toUpperCase()}</Typography></Grid>
+                    <Grid item xs={6}>
+                        <Typography
+                            variant="body1"
+                            fontWeight={600}
+                            fontSize={"1.1rem"}
+                        >
+                            LOCATION
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography>{location.toUpperCase()}</Typography>
+                    </Grid>
 
-                    <Grid item xs={6}><Typography variant="body1" fontWeight={600} fontSize={'1.1rem'}>BEDROOMS</Typography></Grid>
-                    <Grid item xs={6}><Typography>{bedrooms}</Typography></Grid>
+                    <Grid item xs={6}>
+                        <Typography
+                            variant="body1"
+                            fontWeight={600}
+                            fontSize={"1.1rem"}
+                        >
+                            BEDROOMS
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography>{bedrooms}</Typography>
+                    </Grid>
 
-                    <Grid item xs={6}><Typography variant="body1" fontWeight={600} fontSize={'1.1rem'}>AMENITIES</Typography></Grid>
+                    <Grid item xs={6}>
+                        <Typography
+                            variant="body1"
+                            fontWeight={600}
+                            fontSize={"1.1rem"}
+                        >
+                            AMENITIES
+                        </Typography>
+                    </Grid>
                     <Grid item xs={6}>
                         <List sx={{ m: 0, p: 0 }}>
                             {amenities.map((amenity, i) => (
-                                <ListItem key={i} disableGutters disablePadding><ListItemText primary={amenity.toUpperCase()} /></ListItem>
+                                <ListItem key={i} disableGutters disablePadding>
+                                    <ListItemText
+                                        primary={amenity.toUpperCase()}
+                                    />
+                                </ListItem>
                             ))}
-                        </List >
+                        </List>
                     </Grid>
                 </Grid>
             </Grid>
@@ -48,6 +134,6 @@ const Listing = ({ imgSource, status, price, location, bedrooms, amenities }) =>
             {/* In details -> floor size... */}
         </Grid>
     );
-}
+};
 
 export default Listing;
--- resources/js/pages/Listings.jsx
+++ resources/js/pages/Listings.jsx
@@ -10,32 +10,34 @@ import MenuItem from "@mui/material/MenuItem";
 // components
 import SearchBox from "@/components/listings/SearchBox";
 import Listing from "@/components/listings/Listing";
-import Guest from '@/layouts/Guest';
+import Guest from "@/layouts/Guest";
 
 // inertia
-import { Head } from '@inertiajs/inertia-react';
+import { Head } from "@inertiajs/inertia-react";
 
 import { useState } from "react";
 
-const amenities = [
-    "swimming pool",
-    'gym',
-    'garage'
-];
+const amenities = ["swimming pool", "gym", "garage"];
 
 const Listings = ({ listings }) => {
     console.log(listings);
     const [order, setOrder] = useState(1);
     return (
         <Guest>
-            <Head><title>Listings</title></Head>
+            <Head>
+                <title>Listings</title>
+            </Head>
             {/* Search Box */}
             <SearchBox />
 
             <Divider variant="middle" sx={{ my: 2 }} />
 
             {/* Header */}
-            <Box display={'flex'} justifyContent={'space-around'} alignItems={'center'}>
+            <Box
+                display={"flex"}
+                justifyContent={"space-around"}
+                alignItems={"center"}
+            >
                 <Typography variant="h5">Listings Found</Typography>
                 <FormControl sx={{ width: 300 }}>
                     <InputLabel id="order-by">Order By</InputLabel>
@@ -43,7 +45,7 @@ const Listings = ({ listings }) => {
                         labelId="order-by"
                         value={order}
                         label="Order"
-                        onChange={e => setOrder(e.target.value)}
+                        onChange={(e) => setOrder(e.target.value)}
                     >
                         <MenuItem value={1}>Most recent</MenuItem>
                         <MenuItem value={2}>Price: Low to High</MenuItem>
@@ -56,11 +58,23 @@ const Listings = ({ listings }) => {
 
             {/* Listings */}
             {listings.map((listing, i) => (
-                <Listing key={i} imgSource={listing.estate.image ? listing.estate.image : "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"} status={'for sale'} price={100000} location={listing.estate.address} bedrooms={4} amenities={amenities} />
+                <Listing
+                    key={i}
+                    id={listing.estate.id}
+                    imgSource={
+                        listing.estate.image
+                            ? listing.estate.image
+                            : "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"
+                    }
+                    status={"for sale"}
+                    price={100000}
+                    location={listing.estate.address}
+                    bedrooms={4}
+                    amenities={amenities}
+                />
             ))}
-
         </Guest>
     );
-}
+};
 
 export default Listings;
